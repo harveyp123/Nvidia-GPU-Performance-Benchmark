c5cb4dea6e9a055bb625f4774116e706921773c5
HarveyP123 <64821114+HarveyP123@users.noreply.github.com>
2023-07-01 20:27:06 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mix Precision Benchmark
+We test the performance on FP32, FP16 and TF32 data types
---
+++ test_mix_precision.py
@@ -0,0 +1,40 @@
+import torch
+from timm.models.vision_transformer import VisionTransformer
+from timm.models import create_model
+import time
+from torch.cuda.amp import GradScaler, autocast
+
+def train(model, input, amp_enable=False):
+    torch.cuda.synchronize()
+    time_start=time.time()
+    dtype = torch.float16
+    # dtype = torch.bfloat16
+    for i in range(0, 5):
+        with autocast(enabled=amp_enable, dtype=dtype):
+            out=model(input)
+        out.sum().backward()
+
+    torch.cuda.synchronize()
+    print(f'time used: {time.time()-time_start}')
+
+if __name__=='__main__':
+
+    model=create_model('resnet50', #vit_small_patch16_384  vit_large_patch16_384 resnet34
+                       pretrained=False,
+                       num_classes=None,
+                       drop_rate=0,
+                       drop_path_rate=0.3)
+    model.cuda().train()
+    input=torch.rand(256,3,384,384).cuda()
+
+    # warmup, ignore
+    train(model, input)
+
+    print('----train with fp32----')
+    train(model, input)
+    print('----train with autocast----')
+    train(model, input, amp_enable=True)
+    print('----train with tf32----')
+    torch.backends.cuda.matmul.allow_tf32=True
+    torch.backends.cudnn.allow_tf32=True
+    train(model, input)
